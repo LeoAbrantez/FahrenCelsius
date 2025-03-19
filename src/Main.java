ed0942e11addbfc5b52a5beb12dc2360fbff68ff
Leonardo Abrantes <leonardoabrantes1127@gmail.com>
2025-03-19 11:18:11 -0300

    questão do github

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+VAR
+celsius, fahrenheit: Real
+
+INICIO
+
+Ler celsius
+
+fahrenheit ← (celsius * 1.8) + 32
+
+Escreva "O valor transformado é igual a " + fahrenheit + "°F"
+
+FIM
+
+TESTE DE MESA
+
+| Celsius | Fahrenheit (celsius * 1.8 + 32) | Saída |
+|---------|--------------------------------|--------------------------------------------|
+| 0       | 32.0                           | "O valor transformado é igual a 32.0°F"    |
+| 25      | 77.0                           | "O valor transformado é igual a 77.0°F"    |
+| 37      | 98.6                           | "O valor transformado é igual a 98.6°F"    |
+| 100     | 212.0                          | "O valor transformado é igual a 212.0°F"   |
+*/
+
+
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double celsius, fahrenheit;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Strings
+        String scelsius;
+        scelsius = JOptionPane.showInputDialog(null, "Qual o valor em °C?");
+        celsius = Double.parseDouble(scelsius);
+
+        //Main code
+        fahrenheit = (celsius * 1.8) + 32;
+
+        //Saída
+        JOptionPane.showMessageDialog(null, "O valor transformado é igual a " + fahrenheit + "°F" );
     }
 }
